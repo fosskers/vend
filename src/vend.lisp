138ac1fe676205c4dfa3501f06a91c71cee6d679
Colin Woodbury <colin@fosskers.ca>
2025-08-05 13:00:40 +0900

    [vend] bump `--version`

--- src/vend.lisp
+++ src/vend.lisp
@@ -196,7 +196,7 @@ Flags:
 
 (defparameter +vend-rules+
   '((("--help" "-h") 0 (vend/help))
-    ("--version" 0 (format t "0.2.0~%"))
+    ("--version" 0 (format t "0.3.0~%"))
     ("check"  1 (vend/check :focus (cadr 1)) :stop)
     ("eval"   1 (vend/eval (cdr 1)) :stop)
     ("get"    0 (vend/get))
